4d98f3db8b503bd8c89d5f131e8fdd8a78953942
Aleksasha <alexby@t-rex.by>
2016-12-21 18:28:37 +0300

    delete all child nodes for deleted node

--- protected/Node.php
+++ protected/Node.php
@@ -33,10 +33,17 @@ class Node
      */
     public function remove()
     {
+        $idsToDelete = array_map(function($node) {
+            /** @var Node $node */
+            return $node->getId();
+        }, self::getAllChildren($this));
+        $idsToDelete[] = (int)$this->id;
+
         $db = DB::getInstance();
-        $st = $db->prepare('UPDATE nodes SET deleted = 1 WHERE id = :node_id');
-        $st->bindParam('node_id', $this->id);
-        return $st->execute();
+        /**
+         * vars are as plained text (without prepared statement) because all values are int
+         */
+        return $db->exec('UPDATE nodes SET deleted = 1 WHERE id IN (' . implode(', ', $idsToDelete) . ')');
     }
 
     /**
@@ -64,5 +71,20 @@ class Node
         $st = $db->prepare('INSERT INTO nodes (parent_id) VALUES (:parent_id);');
         $st->bindParam('parent_id', $this->id);
         return $st->execute();
+    }
+
+    /**
+     * Recursively get all the children for passed node
+     * @param Node $node
+     * @return Node[]
+     */
+    private static function getAllChildren($node)
+    {
+        $result = [];
+        foreach ($node->getChildren() as $child) {
+            $result[] = $child;
+            $result = array_merge($result, self::getAllChildren($child));
+        }
+        return $result;
     }
 }
